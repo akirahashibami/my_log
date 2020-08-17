b4eef0fd54d0064f840185321380729d55b8966a
akirahashibami <hashibami2207@gmail.com>
2020-08-17 15:43:32 +0000

    [add] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'home#top'
+
+  devise_for :users
+
+  get "users/:id/vlogs" => "users#vlogs", as: "vlog"
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :vlogs, :except => [:new]
+
 end
